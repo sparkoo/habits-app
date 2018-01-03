d573256fabe6e555a03a9cb7942923937af09c8c
Michal Vala <michal.vala@gmail.com>
2018-01-03 07:05:42 +0100

    update firebase config

--- src/firebase_config.ts
+++ src/firebase_config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  authDomain: 'myhabits-e0ba1.firebaseapp.com',
+  databaseURL: 'https://myhabits-e0ba1.firebaseio.com',
+  projectId: 'myhabits-e0ba1',
+  storageBucket: 'myhabits-e0ba1.appspot.com',
+  messagingSenderId: '956324956047'
 };
