6b3ed317e13511551988a80f4c3760b0a57a6af3
Michal Vala <michal.vala@gmail.com>
2018-01-03 06:48:25 +0100

    tsconfig module commonjs, remove useless test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,11 +30,6 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'HabitsApp'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('HabitsApp');
-  }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
